21120d829db83b3001b5865d7cae8c524f93ce9b
Juho Timonen <juho.timonen@aalto.fi>
2020-01-28 09:20:40 +0200

    add result checking app for rna-seq

---
+++ figures/2020/create/app/app.R
@@ -0,0 +1,49 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' in R-studio.
+#
+
+library(shiny)
+library(markdown)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Results"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Table: RNA-seq", icon = icon("columns"), tabName = "table1")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "table1",
+              DT::dataTableOutput("t1")
+      )
+    )
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  # Function for rounding only numeric columns of data frame
+  round_df <- function(df, digits) {
+    is.num      <- vapply(df, is.numeric, FUN.VALUE = logical(1))
+    df[,is.num] <- round(df[,is.num], digits = digits)
+    return(df)
+  }
+  
+  # Data tables
+  output$t1 <- DT::renderDataTable({
+    fn <- normalizePath(file.path('../results.rds'))
+    df <- readRDS(fn)
+    df <- round_df(df, digits = 3)
+    DT::datatable(df)
+  })
+  
+}
+
+shinyApp(ui, server)
+
+
+
