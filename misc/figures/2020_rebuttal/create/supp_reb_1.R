0b147adad9696625a3df77041c23d9c679c98206
Juho Timonen <juho.timonen@aalto.fi>
2020-11-29 03:53:18 +0200

    add noise testing results

---
+++ misc/figures/2020_rebuttal/create/supp_reb_1.R
@@ -0,0 +1,16 @@
+
+res <- readRDS('rocs_1_to_8.rds')
+
+auc1 <- rep(0, 8)
+auc2 <- rep(0, 8)
+for (j in 1:8) {
+  r <- res[[j]]$ROC
+  auc1[j] <- r[[1]]$auc
+  auc2[j] <- r[[2]]$auc
+}
+
+snr <- c(0.05, 0.1, 0.5, 1, 2, 5, 10, 20)
+plot(snr, auc1, 'o', ylim = c(0,1), 'ylab' = "AUC", 
+     xlab = 'Signal-to-noise ratio', pch = 16, xlim = c(0, 10))
+lines(snr, auc2, col = "firebrick3")
+points(snr, auc2, col = "firebrick3", pch = 16)
